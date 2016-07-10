dedf611354d33bda4cf44386873a293b211495e4
bsq <adquang@gmail.com>
2016-07-10 23:14:58 +0700

    no message

--- org.js
+++ org.js
@@ -125,10 +125,10 @@ function scan() {
         
         
         
-          //alert("We got a barcode\n" +
-          //      "Result: " + result.text + "\n" +
-          //      "Format: " + result.format + "\n" +
-          //      "Cancelled: " + result.cancelled);
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
       }, 
       function (error) {
           alert("Scanning failed: " + error);
