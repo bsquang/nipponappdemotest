d3c258be4cf33f8e153a9ac2c7e57734c4f40030
bsq <adquang@gmail.com>
2016-07-10 23:27:08 +0700

    no message

--- org.js
+++ org.js
@@ -132,11 +132,11 @@ function scan() {
         }
         
         
-        
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
+          //
+          //alert("We got a barcode\n" +
+          //      "Result: " + result.text + "\n" +
+          //      "Format: " + result.format + "\n" +
+          //      "Cancelled: " + result.cancelled);
       }, 
       function (error) {
           alert("Scanning failed: " + error);
