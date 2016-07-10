c072a3752488b378e07b7fcc54f8071c690e66ef
bsq <adquang@gmail.com>
2016-07-10 13:20:16 +0700

    Add new plugin barcode scanner

--- org.js
+++ org.js
@@ -41,17 +41,20 @@ function logout() {
 
 function scan() {
     
-    cordova.plugins.barcodeScanner.scan(
-      function (result) {        
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      }, 
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-   );
+    
+    scanner.startScanning()
+    
+   // cordova.plugins.barcodeScanner.scan(
+   //   function (result) {        
+   //       alert("We got a barcode\n" +
+   //             "Result: " + result.text + "\n" +
+   //             "Format: " + result.format + "\n" +
+   //             "Cancelled: " + result.cancelled);
+   //   }, 
+   //   function (error) {
+   //       alert("Scanning failed: " + error);
+   //   }
+   //);
     
     
 }
