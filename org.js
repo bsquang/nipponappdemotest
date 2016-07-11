d1f5e0060b30531cf20e0ece8888c234b7b408d5
bsq <adquang@gmail.com>
2016-07-11 12:28:57 +0700

    no message

--- org.js
+++ org.js
@@ -229,7 +229,7 @@ document.addEventListener('deviceready', function(){
 
 document.addEventListener('push-notification', function(event) {
              var notification = event.notification;
-             alert(notification.aps.alert);
+             //alert(notification.aps.alert);
              pushNotification.setApplicationIconBadgeNumber(0);
 });
 
@@ -242,7 +242,7 @@ function initPushwoosh() {
                                 var notification = event.notification;
  
                                 //display alert to the user for example
-                                alert(notification.aps.alert);
+                                //alert(notification.aps.alert);
                                
                                 //clear the app badge
                                 pushNotification.setApplicationIconBadgeNumber(0);
