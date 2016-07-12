4528fd5e2429c5ca504ef13b343dfd5959c2fd3e
bsq <adquang@gmail.com>
2016-07-12 15:01:40 +0700

    no message

--- org.js
+++ org.js
@@ -249,7 +249,7 @@ window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
 function initPushwooshANDROID()
 {
     alert("here android notification0s")
-    var pushNotification = cordova.plugins.pushNotification;
+    var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
     
     alert("here android notification1")
  
